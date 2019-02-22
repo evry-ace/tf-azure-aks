e8c635462abe61f492e0600f35e43c8431f06fb3
Hans Kristian Flaatten <hans.flaatten@evry.com>
2019-02-22 10:55:03 +0100

    Add required network_plugin to network_profile block

--- aks.tf
+++ aks.tf
@@ -45,6 +45,7 @@ resource "azurerm_kubernetes_cluster" "k8s_cluster" {
   }
 
   network_profile {
+    network_plugin = "${var.aks_network_plugin}"
     pod_cidr = "${var.aks_pod_cidr}"
     service_cidr = "${var.aks_service_cidr}"
     dns_service_ip = "${var.aks_dns_service_ip}"
