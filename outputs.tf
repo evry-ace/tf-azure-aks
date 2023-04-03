a2bf98f4371fa264d82aa062ba2eb778179663ad
Bjørge Solli <bjorge.solli@tietoevry.com>
2023-04-03 16:05:48 +0200

    output fqdn

--- outputs.tf
+++ outputs.tf
@@ -83,6 +83,10 @@ output "private_fqdn" {
   value = azurerm_kubernetes_cluster.k8s_cluster.private_fqdn
 }
 
+output "fqdn" {
+  value = azurerm_kubernetes_cluster.k8s_cluster.fqdn
+}
+
 output "oidc_issuer_url" {
   value = azurerm_kubernetes_cluster.k8s_cluster.oidc_issuer_url
 }
